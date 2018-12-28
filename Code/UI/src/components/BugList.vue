44b2454799e158ebb09ab831d6b928a4b8869a0a
michaelsoft <michaellyk@qq.com>
2018-12-28 19:27:39 +0800

    grid

---
+++ Code/UI/src/components/BugList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="buglist">
+    <table class="table table-striped table-bordered table-hover table-condensed">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Tittle</th>
+          <th>Severity</th>
+          <th>Priority</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr >
+          <td>1</td>
+          <td>Bangalore</td>
+          <td>High</td>
+          <td>High</td>
+          <td>New</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Bangalore</td>
+          <td>High</td>
+          <td>High</td>
+          <td>Fixed</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Bangalore</td>
+          <td>High</td>
+          <td>High</td>
+          <td>Assigned</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BugList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
