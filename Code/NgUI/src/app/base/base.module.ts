26a515a301300adef697e668de53adaaf6684583
michaelsoft <michaellyk@qq.com>
2019-01-30 19:36:23 +0800

    Login

--- Code/NgUI/src/app/base/base.module.ts
+++ Code/NgUI/src/app/base/base.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu.component';
 import { LoginInfoComponent } from './login-info.component';
 import { FooterComponent } from './footer.component';
+import { LoginComponent } from './login.component';
 
 @NgModule({
-  declarations: [MenuComponent, LoginInfoComponent, FooterComponent],
+  declarations: [MenuComponent, LoginInfoComponent, FooterComponent, LoginComponent],
   imports: [
     CommonModule
   ],
-  exports: [ MenuComponent, LoginInfoComponent, FooterComponent ]
+  exports: [ MenuComponent, LoginInfoComponent, FooterComponent, LoginComponent ]
 })
 export class BaseModule { }
