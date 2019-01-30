c0176513f076b70b4537f4aabc54a3941dc79fc3
michaelsoft <michaellyk@qq.com>
2019-01-30 19:20:24 +0800

    Tabs

--- Code/NgUI/src/app/bug/bug.module.ts
+++ Code/NgUI/src/app/bug/bug.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
+
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 import { CommonModule } from '@angular/common';
 import { BugListComponent } from './bug-list.component';
 import { BugDetailComponent } from './bug-detail.component';
 import { BugSearchComponent } from './bug-search.component';
 import { BugFilterComponent } from './bug-filter.component';
 
+
 @NgModule({
   declarations: [BugListComponent, BugDetailComponent, BugSearchComponent, BugFilterComponent],
   imports: [
+    TabsModule.forRoot(),
     CommonModule
   ],
   exports: [BugListComponent, BugDetailComponent, BugSearchComponent, BugFilterComponent],
